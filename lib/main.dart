46504a943cc33f9bb11114edf87ac63f854ececb
Talhaasan <44949936+Talhaasan@users.noreply.github.com>
2020-10-29 14:26:15 +0300

    last version

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,6 @@ import 'mainscreen.dart';
 
 void main() => runApp(MaterialApp(home: MyApp()));
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(home: HomeScreen());
-//   }
-// }
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -36,52 +29,3 @@ class _MyAppState extends State<MyApp> {
                     fit: BoxFit.cover))));
   }
 }
-
-// class HomeScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.deepPurple[200],
-//      appBar: AppBar(
-//         title: Text("KDV Hesaplama"),
-//         backgroundColor: Colors.deepPurple[700],
-//       ),
-//       body: Container(
-//         child: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: <Widget>[
-//               RaisedButton(
-//                   child: Text("  Hesap Makinesi  "),
-//                   onPressed: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(builder: (context) => CalculatorPage()),
-//                     );
-//                   },
-//                   color: Colors.deepPurple[700],
-//                   highlightColor: Colors.red[400],
-//                   textColor: Colors.white,
-//                   padding: EdgeInsets.fromLTRB(10, 10, 10, 10)),
-//               SizedBox(height: 10),
-//               RaisedButton(
-//                   child: Text("  KDV Hesaplama  "),
-//                   onPressed: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                           builder: (context) => KDVCalculatorPage()),
-//                     );
-//                   },
-//                   color: Colors.deepPurple[700],
-//                   highlightColor: Colors.red[400],
-//                   textColor: Colors.white,
-//                   padding: EdgeInsets.fromLTRB(10, 10, 10, 10)),
-
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
